0bcc084bffed925f118d075e2419290f1f20d976
Ken Ojiri <kojiri@vmware.com>
2024-02-05 09:17:42 +0000

    fix aws-concourse/0-outputs.tf to remove ELB external IPs from Terraform outputs

--- aws-concourse/0-outputs.tf
+++ aws-concourse/0-outputs.tf
@@ -6,7 +6,6 @@ locals {
     plane_subnet_ids = var.subnet_ids
     plane_subnet_cidrs = [data.aws_subnet.a.cidr_block,data.aws_subnet.b.cidr_block,data.aws_subnet.c.cidr_block]
     plane_availability_zones = [data.aws_subnet.a.availability_zone,data.aws_subnet.b.availability_zone,data.aws_subnet.c.availability_zone]
-    elb_external_ips = ["${cidrhost(data.aws_subnet.a.cidr_block, 10)}","${cidrhost(data.aws_subnet.b.cidr_block, 10)}","${cidrhost(data.aws_subnet.c.cidr_block ,10)}"]
     bosh_security_group_id = aws_security_group.plane.id
     concourse_web_security_group_ids = [ aws_security_group.concourse.id, aws_security_group.plane.id]
     concourse_worker_security_group_ids = [ aws_security_group.plane.id ]
