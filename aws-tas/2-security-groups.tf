8205184b655458b0b114fcf827525d4787a9c795
Ken Ojiri <kojiri@vmware.com>
2024-02-05 08:02:28 +0000

    add TAS on AWS

---
+++ aws-tas/2-security-groups.tf
@@ -0,0 +1,155 @@
+## inputs
+## - environment_name
+
+resource "aws_security_group" "plane" {
+  name   = "${var.environment_name}-plane-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = [data.aws_vpc.foundation.cidr_block]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-plane-sg" }
+}
+
+resource "aws_security_group" "opsman" {
+  name   = "${var.environment_name}-opsman-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 22
+    to_port     = 22
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-opsman-sg" }
+}
+
+resource "aws_security_group" "tas_router" {
+  name   = "${var.environment_name}-tas-router-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-tas-router-sg" }
+}
+
+resource "aws_security_group" "tas_sshproxy" {
+  name   = "${var.environment_name}-tas-sshproxy-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 2222
+    to_port     = 2222
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-tas-sshproxy-sg" }
+}
+
+resource "aws_security_group" "tas_tcprouter" {
+  name   = "${var.environment_name}-tas-tcprouter-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "tcp"
+    from_port   = 1024
+    to_port     = 1123
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-tas-tcprouter-sg" }
+}
+
+resource "aws_security_group" "mysql" {
+  name   = "${var.environment_name}-mysql-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = [data.aws_vpc.foundation.cidr_block]
+    protocol    = "tcp"
+    from_port   = 3306
+    to_port     = 3306
+  }
+
+  egress {
+    cidr_blocks = [data.aws_vpc.foundation.cidr_block]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-mysql-sg" }
+}
+
+## outputs
+## - opsman_security_group_ids = [ aws_security_group.plane.id, aws_security_group.opsman.id ]
+## - tas_router_security_group_ids = [ aws_security_group.tas_router.id, aws_security_group.plane.id]
+## - tas_sshproxy_security_group_ids = [ aws_security_group.tas_sshproxy.id, aws_security_group.plane.id]
+## - tas_tcprouter_security_group_ids = [ aws_security_group.tas_tcprouter.id, aws_security_group.plane.id]
