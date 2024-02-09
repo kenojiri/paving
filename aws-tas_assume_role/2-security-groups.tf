9eb92a00a1f714ca6916ca9466da123aa397c21f
Ken Ojiri <kojiri@vmware.com>
2024-02-09 08:09:53 +0000

    WIP

--- aws-tas_assume_role/2-security-groups.tf
+++ aws-tas_assume_role/2-security-groups.tf
@@ -88,92 +88,92 @@ resource "aws_security_group" "web_lb" {
   tags = { "Name" = "${var.environment_name}-web-lb-sg" }
 }
 
-#resource "aws_security_group" "tas_router" {
-#  provider = aws.target
-#  name   = "${var.environment_name}-tas-router-sg"
-#  vpc_id = data.aws_vpc.foundation.id
-#
-#  ingress {
-#    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
-#    protocol    = "tcp"
-#    from_port   = 443
-#    to_port     = 443
-#  }
-#
-#  ingress {
-#    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
-#    protocol    = "tcp"
-#    from_port   = 80
-#    to_port     = 80
-#  }
-#
-#  egress {
-#    cidr_blocks = ["0.0.0.0/0"]
-#    protocol    = "-1"
-#    from_port   = 0
-#    to_port     = 0
-#  }
-#
-#  tags = { "Name" = "${var.environment_name}-tas-router-sg" }
-#}
-#
-#resource "aws_security_group" "tas_sshproxy" {
-#  provider = aws.target
-#  name   = "${var.environment_name}-tas-sshproxy-sg"
-#  vpc_id = data.aws_vpc.foundation.id
-#
-#  ingress {
-#    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
-#    protocol    = "tcp"
-#    from_port   = 2222
-#    to_port     = 2222
-#  }
-#
-#  egress {
-#    cidr_blocks = ["0.0.0.0/0"]
-#    protocol    = "-1"
-#    from_port   = 0
-#    to_port     = 0
-#  }
-#
-#  tags = { "Name" = "${var.environment_name}-tas-sshproxy-sg" }
-#}
-#
-#resource "aws_security_group" "tas_tcprouter" {
-#  provider = aws.target
-#  name   = "${var.environment_name}-tas-tcprouter-sg"
-#  vpc_id = data.aws_vpc.foundation.id
-#
-#  ingress {
-#    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
-#    protocol    = "tcp"
-#    from_port   = 26770
-#    to_port     = 26809
-#  }
-#
-#  ingress {
-#    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
-#    protocol    = "tcp"
-#    from_port   = 15692
-#    to_port     = 15692
-#  }
-#
-#  ingress {
-#    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
-#    protocol    = "tcp"
-#    from_port   = 1024
-#    to_port     = 1024
-#  }
-#
-#  egress {
-#    cidr_blocks = ["0.0.0.0/0"]
-#    protocol    = "-1"
-#    from_port   = 0
-#    to_port     = 0
-#  }
-#
-#  tags = { "Name" = "${var.environment_name}-tas-tcprouter-sg" }
-#}
+resource "aws_security_group" "tas_router" {
+  provider = aws.target
+  name   = "${var.environment_name}-tas-router-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+  }
+
+  ingress {
+    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-tas-router-sg" }
+}
+
+resource "aws_security_group" "tas_sshproxy" {
+  provider = aws.target
+  name   = "${var.environment_name}-tas-sshproxy-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 2222
+    to_port     = 2222
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-tas-sshproxy-sg" }
+}
+
+resource "aws_security_group" "tas_tcprouter" {
+  provider = aws.target
+  name   = "${var.environment_name}-tas-tcprouter-sg"
+  vpc_id = data.aws_vpc.foundation.id
+
+  ingress {
+    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 26770
+    to_port     = 26809
+  }
+
+  ingress {
+    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 15692
+    to_port     = 15692
+  }
+
+  ingress {
+    cidr_blocks = ["10.0.0.0/16","100.99.0.0/16"]
+    protocol    = "tcp"
+    from_port   = 1024
+    to_port     = 1024
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+  }
+
+  tags = { "Name" = "${var.environment_name}-tas-tcprouter-sg" }
+}
 
 resource "aws_security_group" "mysql" {
   provider = aws.target
