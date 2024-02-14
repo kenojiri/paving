0e1a588b7dbfba7525e1c9666cce22febe3996c9
Ken Ojiri <kojiri@vmware.com>
2024-02-14 07:26:17 +0000

    add ALB/NLB

--- aws-tas/0-outputs.tf
+++ aws-tas/0-outputs.tf
@@ -1,7 +1,5 @@
 locals {
   config = {
-    opsman_subnet_id = data.aws_subnet.a.id
-    opsman_private_ip = "${cidrhost(data.aws_subnet.a.cidr_block, 10)}"
     opsman_security_group_ids = [ aws_security_group.plane.id, aws_security_group.opsman.id ]
     plane_subnet_ids = var.subnet_ids
     plane_subnet_cidrs = [data.aws_subnet.a.cidr_block,data.aws_subnet.b.cidr_block,data.aws_subnet.c.cidr_block]
@@ -17,11 +15,11 @@ locals {
     tas_router_security_group_ids = [ aws_security_group.tas_router.id, aws_security_group.plane.id]
     tas_sshproxy_security_group_ids = [ aws_security_group.tas_sshproxy.id, aws_security_group.plane.id]
     tas_tcprouter_security_group_ids = [ aws_security_group.tas_tcprouter.id, aws_security_group.plane.id]
-    #tas_lb_external_fqdn = aws_lb.tas.dns_name
-    #tas_router_https_target_group_name = aws_lb_target_group.web-443.name
-    #tas_router_http_target_group_name = aws_lb_target_group.web-80.name
-    #tas_sshproxy_target_group_name = aws_lb_target_group.ssh-2222.name
-    #tas_tcp_target_group_names = aws_lb_target_group.tcprouter[*].name
+    tas_web_lb_external_fqdn = aws_lb.tas_alb.dns_name
+    tas_tcp_lb_external_fqdn = aws_lb.tas_nlb.dns_name
+    tas_router_https_target_group_name = aws_lb_target_group.web-443.name
+    tas_sshproxy_target_group_name = aws_lb_target_group.ssh-2222.name
+    tas_tcprouter_target_group_names = [aws_lb_target_group.tcp-1024.name,aws_lb_target_group.tcp-15692.name,aws_lb_target_group.rabbitmq[*].name]
   }
 }
 
